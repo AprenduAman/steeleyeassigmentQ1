387f5a7ec56389da397a9da3a1b7c90948f1420c
Aprendu Aman <95740633+AprenduAman@users.noreply.github.com>
2023-08-06 20:00:38 +0530

    Add files via upload

---
+++ src/component/list/ListRow.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./ListRow.module.css";
+
+const ListCell = ({ children, onClick }) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <tr className={styles.cell} onClick={handleClick}>
+      {children}
+    </tr>
+  );
+};
+
+export default ListCell;
